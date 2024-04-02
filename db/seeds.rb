ae921c6ae6c87f54dc14aef53e37135d32c2d2ce
Esteban Alzate <estebansa@outlook.com>
2024-04-01 22:57:51 -0500

    feature: create seeds records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,185 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Sample books
+books = [
+  {
+    title: 'Cien años de soledad',
+    author: 'Gabriel García Márquez',
+    description: 'Una de las obras cumbres de la literatura hispanoamericana. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..500),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'El amor en los tiempos del cólera',
+    author: 'Gabriel García Márquez',
+    description: 'Una historia de amor ambientada en la costa caribeña de Colombia. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..500),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Crónica de una muerte anunciada',
+    author: 'Gabriel García Márquez',
+    description: 'Una novela corta que explora temas de honor y venganza en un pequeño pueblo latinoamericano. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(100..200),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'El otoño del patriarca',
+    author: 'Gabriel García Márquez',
+    description: 'Una obra de García Márquez que retrata la dictadura y el poder absoluto. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(150..300),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Ficciones',
+    author: 'Jorge Luis Borges',
+    description: 'Una colección de cuentos que desafían los límites de la realidad. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(150..300),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'El Aleph',
+    author: 'Jorge Luis Borges',
+    description: 'Un libro de cuentos donde se exploran temas de infinito y eternidad. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(150..300),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'El libro de arena',
+    author: 'Jorge Luis Borges',
+    description: 'Una colección de cuentos que exploran temas de la metafísica y el infinito. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(150..300),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Fervor de Buenos Aires',
+    author: 'Jorge Luis Borges',
+    description: 'El primer libro de poesía publicado por Borges. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(5..30),
+    pages_number: rand(50..150),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'La ciudad y los perros',
+    author: 'Mario Vargas Llosa',
+    description: 'Una novela que retrata la brutalidad de la vida en un colegio militar de Lima. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(250..400),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'La casa verde',
+    author: 'Mario Vargas Llosa',
+    description: 'Un relato épico que abarca varias décadas en la Amazonía peruana. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(250..400),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Conversación en la catedral',
+    author: 'Mario Vargas Llosa',
+    description: 'Una novela compleja que explora temas de identidad y corrupción política. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(250..400),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Pantaleón y las visitadoras',
+    author: 'Mario Vargas Llosa',
+    description: 'Una sátira sobre la vida militar en la selva peruana. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..350),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Rayuela',
+    author: 'Julio Cortázar',
+    description: 'Una novela experimental que desafía las convenciones narrativas. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(300..600),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Cronopios y famas',
+    author: 'Julio Cortázar',
+    description: 'Una colección de relatos breves que muestran la imaginación desbordante de Cortázar. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..400),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Historias de cronopios y de famas',
+    author: 'Julio Cortázar',
+    description: 'Otra colección de cuentos que explora el mundo surrealista de Cortázar. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..400),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Final del juego',
+    author: 'Julio Cortázar',
+    description: 'Una colección de cuentos cortos que incluye algunos de los relatos más famosos de Cortázar. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(150..300),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Veinte poemas de amor y una canción desesperada',
+    author: 'Pablo Neruda',
+    description: 'Uno de los libros de poesía más emblemáticos de Neruda. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(50..150),
+    stock_quantity: rand(10..100)
+  },
+  {
+    title: 'Canto general',
+    author: 'Pablo Neruda',
+    description: 'Un extenso poema épico que abarca la historia y geografía de América Latina. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, illo itaque. Iusto voluptatibus tenetur maxime rerum perferendis, dignissimos numquam praesentium totam ducimus quia quidem accusamus laborum aspernatur, libero sapiente eos?',
+    price: rand(10..50),
+    pages_number: rand(200..500),
+    stock_quantity: rand(10..100)
+  }
+]
+
+# Basic users
+users = [
+  {
+    full_name: 'Admin user',
+    email: 'admin@mail.com',
+    password: '!Admin123',
+    password_confirmation: '!Admin123',
+    admin: true
+  },
+  {
+    full_name: 'Pepe Perez 1',
+    email: 'pepe1@mail.com',
+    password: '!Test123',
+    password_confirmation: '!Test123',
+    admin: false
+  },
+  {
+    full_name: 'Pepe Perez 2',
+    email: 'pepe2@mail.com',
+    password: '!Test123',
+    password_confirmation: '!Test123',
+    admin: false
+  },
+  {
+    full_name: 'Pepe Perez 3',
+    email: 'pepe3@mail.com',
+    password: '!Test123',
+    password_confirmation: '!Test123',
+    admin: false
+  }
+]
+
+Book.upsert_all(books, unique_by: %i[title author])
+
+users.each do |user_attributes|
+  User.find_or_initialize_by(email: user_attributes[:email]).update(user_attributes)
+end
