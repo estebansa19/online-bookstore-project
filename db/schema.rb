4ab18998fd5628f2453837522a6e93b5ed6e0a0d
Esteban Alzate <estebansa@outlook.com>
2024-04-01 22:57:04 -0500

    feature: add User model and configure devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_012459) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_015147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_012459) do
     t.datetime "updated_at", null: false
     t.index ["title", "author"], name: "index_books_on_title_and_author", unique: true
     t.index ["title"], name: "index_books_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean "admin", default: false, null: false
+    t.string "full_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
